d8ee0634e0e4c1f423cec795cc1f43c5867bdf30
Charlie Hitselberger <chitselb@gmail.com>
2010-08-29 13:17:46 -0400

    removing Y=1 C=clear contract from NEXT

--- pettil.sh
+++ pettil.sh
@@ -4,7 +4,7 @@ alias xap='xa -x pettil.a65;xa pettil.a65;./pettil.sh'
 sed 's/^\([a-zA-Z0-9]*\),\ 0x\(....\).*$/al C\:\2 \.\1/g' < pettil.lab > t.lab
 sort t.lab > pettil.lab
 rm t.lab
-echo break .cold2>>pettil.lab
+echo break .xpetp>>pettil.lab
 echo fill 0 8c 0 >>pettil.lab
 echo fill .stackl 8c ad >>pettil.lab
 echo fill .stackh 8c de >>pettil.lab
