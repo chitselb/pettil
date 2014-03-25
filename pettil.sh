72566ec86ae3223b0f034cd7f213e979fc8021d3
Charlie Hitselberger <chitselb@gmail.com>
2014-03-25 00:59:15 -0400

    did a lot of juggling, this has a working inner/outer interpreter

--- pettil.sh
+++ pettil.sh
@@ -2,28 +2,14 @@
 # pettil.sh
 alias xpetp='xpet -moncommand pettil.lab pettil.obj'
 alias xap='xa -x pettil.a65;xa pettil.a65;./pettil.sh'
-sed 's/^\([_a-zA-Z0-9]*\),\ 0x\(....\).*$/al C\:\2 \.\1/g' < pettil.lab > t.lab
-sort t.lab > pettil.lab
-rm t.lab
-echo break .cold>>pettil.lab
-#echo break ._query>>pettil.lab
-echo break ._interpret>>pettil.lab
-echo break .foo>>pettil.lab
-echo break .bar>>pettil.lab
-#echo break f563>>pettil.lab
+sed 's/^\([_a-zA-Z0-9]*\),\ 0x\(....\).*$/al C\:\2 \.\1/g' < pettil.lab | sort > t.lab
+mv t.lab pettil.lab
+echo watch store .userarea .setbrk>>pettil.lab
 #echo break .cold>>pettil.lab
+echo break ._pdq>>pettil.lab
+echo break ._interpret>>pettil.lab
 #echo break .nexto>>pettil.lab
-#echo break .nexto>>pettil.lab
-#echo "disable 1">>pettil.lab
 #echo break .exit>>pettil.lab
-#echo "disable 2">>pettil.lab
-#echo break .SW16B>>pettil.lab
-#echo "disable 3">>pettil.lab
-#echo break .NUL>>pettil.lab
-#echo break .SW16>>pettil.lab
-# echo break .foo>>pettil.lab
-# echo break .test>>pettil.lab
-# echo break .plit>>pettil.lab
 
 # This ROM routine tests the STOP key and writes to $0010.  Very bad!
 # echo break f935>> pettil.lab
