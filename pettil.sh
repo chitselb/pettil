c331f0bc462214d84d3c2d30a78dfadc027d528c
Charlie Hitselberger <chitselb@gmail.com>
2014-03-26 18:55:12 -0400

    broken interpreter

--- pettil.sh
+++ pettil.sh
@@ -4,10 +4,11 @@ alias xpetp='xpet -moncommand pettil.lab pettil.obj'
 alias xap='xa -x pettil.a65;xa pettil.a65;./pettil.sh'
 sed 's/^\([_a-zA-Z0-9]*\),\ 0x\(....\).*$/al C\:\2 \.\1/g' < pettil.lab | sort > t.lab
 mv t.lab pettil.lab
-echo watch store .userarea .setbrk>>pettil.lab
-#echo break .cold>>pettil.lab
-echo break ._pdq>>pettil.lab
-echo break ._interpret>>pettil.lab
+#echo watch store .userarea .setbrk>>pettil.lab
+echo break .cold>>pettil.lab
+echo break .xyzzy>>pettil.lab
+#echo break ._pdq>>pettil.lab
+#echo break ._interpret>>pettil.lab
 #echo break .nexto>>pettil.lab
 #echo break .exit>>pettil.lab
 
