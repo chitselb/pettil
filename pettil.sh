194490c97df0ee76bc4e3de55f499cfca477e8b7
Charlie Hitselberger <chitselb@gmail.com>
2014-03-31 19:38:47 -0400

    ...

--- pettil.sh
+++ pettil.sh
@@ -4,10 +4,12 @@ alias xpetp='xpet -moncommand pettil.lab pettil.obj'
 alias xap='xa -x pettil.a65;xa pettil.a65;./pettil.sh'
 sed 's/^\([_a-zA-Z0-9]*\),\ 0x\(....\).*$/al C\:\2 \.\1/g' < pettil.lab | sort > t.lab
 mv t.lab pettil.lab
-echo break .BK>>pettil.lab
-echo break .PULL>>pettil.lab
 echo break .npfind>>pettil.lab
 echo break .pfind>>pettil.lab
+echo break .disp1>>pettil.lab
+echo break .disp2>>pettil.lab
+echo "disable 3">>pettil.lab
+echo "disable 4">>pettil.lab
 #echo break .cold>>pettil.lab
 #echo watch store .userarea .setbrk>>pettil.lab
 #echo break .cold>>pettil.lab
