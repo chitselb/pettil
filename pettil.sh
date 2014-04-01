d4120154ea00692b660aaf2fa1ad3251fe2ebdc3
Charlie Hitselberger <chitselb@gmail.com>
2014-04-01 15:10:41 -0400

    rewrite (FIND) in modified Sweet16.  Things are still a little borked in the outer interpreter

--- pettil.sh
+++ pettil.sh
@@ -4,20 +4,20 @@ alias xpetp='xpet -moncommand pettil.lab pettil.obj'
 alias xap='xa -x pettil.a65;xa pettil.a65;./pettil.sh'
 sed 's/^\([_a-zA-Z0-9]*\),\ 0x\(....\).*$/al C\:\2 \.\1/g' < pettil.lab | sort > t.lab
 mv t.lab pettil.lab
-echo break .strcomp16>>pettil.lab
+#echo break .strcomp16>>pettil.lab
 #echo break .strcmp16>>pettil.lab
-echo break .opfind>>pettil.lab
-echo break .pfind>>pettil.lab
-echo break .disp1>>pettil.lab
-echo break .disp2>>pettil.lab
+#echo break .opfind>>pettil.lab
+#echo break .pfind>>pettil.lab
+#echo break .disp1>>pettil.lab
+#echo break .disp2>>pettil.lab
 #echo "disable 3">>pettil.lab
 #echo "disable 4">>pettil.lab
 #echo break .getpstack>>pettil.lab
 #echo break .putpstack>>pettil.lab
 #echo watch store .userarea .setbrk>>pettil.lab
 #echo break .cold>>pettil.lab
-echo break .xyzzy>>pettil.lab
-echo break .plugh>>pettil.lab
+#echo break .xyzzy>>pettil.lab
+#echo break .plugh>>pettil.lab
 #echo break ._pdq>>pettil.lab
 #echo break ._interpret>>pettil.lab
 #echo break .nexto>>pettil.lab
