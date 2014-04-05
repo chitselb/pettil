2b193fe8ca0096120f94b09e0ea3ce8dc184c377
Charlie Hitselberger <chitselb@gmail.com>
2014-04-05 05:15:44 -0400

    fixed some things, broke some other things.  outer interpreter is hosed, problem is in user variables

--- pettil.sh
+++ pettil.sh
@@ -4,8 +4,8 @@ alias xpetp='xpet -moncommand pettil.lab pettil.obj'
 alias xap='xa -x pettil.a65;xa pettil.a65;./pettil.sh'
 sed 's/^\([_a-zA-Z0-9]*\),\ 0x\(....\).*$/al C\:\2 \.\1/g' < pettil.lab | sort > t.lab
 mv t.lab pettil.lab
-echo break ._interpret>>pettil.lab
-#echo break .cold>>pettil.lab
+echo break .state>>pettil.lab
+#echo break ._rethread>>pettil.lab
 #echo break .strcmp16>>pettil.lab
 #echo break .opfind>>pettil.lab
 #echo break .pfind>>pettil.lab
