b8097960c1db8ce2a4b440d28644697d5bc6fb38
Charlie Hitselberger <chitselb@gmail.com>
2014-04-27 12:47:46 -0400

    working on linewrap table primitives in screen editor

--- pettil.sh
+++ pettil.sh
@@ -10,6 +10,7 @@ echo break .exit>>pettil.lab	# debugging secondaries
 echo "disable 2">>pettil.lab
 #echo "watch store 2 3">>pettil.lab	# UP
 echo break .xyzzy>>pettil.lab
+echo break .dminus>>pettil.lab
 #echo break .TOBR>>pettil.lab
 #echo break .SW16>>pettil.lab
 #echo break .RTN>>pettil.lab
