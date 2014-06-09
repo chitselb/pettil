50991acedce0b6eb679424c8740f9e3e1e5ffac6
Charlie Hitselberger <chitselb@gmail.com>
2014-06-09 09:37:09 -0400

    Find is working, without vocabularies yet

--- symtab.rb
+++ symtab.rb
@@ -93,7 +93,7 @@
     
     # build a label file so pettil-tdict.a65 can find things in core
 	# fix problems with reserved words e.g. bc add in hex address
-	use_decimal = ' spaces02 cr01 twofetch _mkpkt '  
+	use_decimal = ' udslashmod dodoes01 spaces01 rlencode04 '  
 	always_use_decimal = false
     symfile = File.open("pettil-core.def",'w') do |f|
 #       symfile.write(#{a[0]}=#{a[1]}\n")
