5f1a9357a45270cb41865fbebcb4843770ff909b
Charlie Hitselberger <chitselb@gmail.com>
2014-06-11 00:22:59 -0400

    NUMBER works, removed vestigial CONVERT word

--- symtab.rb
+++ symtab.rb
@@ -93,7 +93,7 @@
     
     # build a label file so pettil-tdict.a65 can find things in core
 	# fix problems with reserved words e.g. bc add in hex address
-	use_decimal = ' uarea usercold uservmbuf zgt01 _pudot '  
+	use_decimal = ' uarea usercold uservmbuf zgt01 _pudot dlt02 '  
 	always_use_decimal = false
     symfile = File.open("pettil-core.def",'w') do |f|
 #       symfile.write(#{a[0]}=#{a[1]}\n")
