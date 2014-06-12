589271a0bb6add9c95f65c57d0235ef553799d12
Charlie Hitselberger <chitselb@gmail.com>
2014-06-11 20:33:08 -0400

    tablet

--- symtab.rb
+++ symtab.rb
@@ -94,7 +94,7 @@
     
     # build a label file so pettil-tdict.a65 can find things in core
 	# fix problems with reserved words e.g. bc add in hex address
-	use_decimal = ' uarea usercold uservmbuf zgt01 _pudot dlt02 pushya spaces01 _word '  
+	use_decimal = ' uarea usercold uservmbuf zgt01 _pudot dlt02 pushya spaces01 _word _space '  
 	always_use_decimal = false
     symfile = File.open("pettil-core.def",'w') do |f|
 #       symfile.write(#{a[0]}=#{a[1]}\n")
