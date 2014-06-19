980c85eb15648842a37c8f339d042ae63d7c8840
Charlie Hitselberger <chitselb@gmail.com>
2014-06-18 21:35:22 -0400

    changed to tiddlywiki 5

--- symtab.rb
+++ symtab.rb
@@ -94,7 +94,7 @@
 
     # build a label file so pettil-tdict.a65 can find things in core
     # fix problems with reserved words e.g. bc add in hex address
-    use_decimal = ' pushya number02 '
+    use_decimal = ' pushya number02 i usercurrent userarea userup '
     always_use_decimal = false
     symfile = File.open("pettil-core.def",'w') do |f|
 #       symfile.write(#{a[0]}=#{a[1]}\n")
