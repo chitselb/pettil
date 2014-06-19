102ce22351da7064fa18686d7a7d501df103029b
Charlie Hitselberger <chitselb@gmail.com>
2014-06-19 07:38:04 -0400

    editor is stabilized again.  screen navigation works.  fixed CMOVE> and put +MOVE in core

--- symtab.rb
+++ symtab.rb
@@ -94,7 +94,7 @@
 
     # build a label file so pettil-tdict.a65 can find things in core
     # fix problems with reserved words e.g. bc add in hex address
-    use_decimal = ' pushya number02 i usercurrent userarea userup _symbuf '
+    use_decimal = ' pushya number02 i usercurrent userarea userup _symbuf type02 _pdot _pktplus warm02 doslashmod '
     always_use_decimal = false
     symfile = File.open("pettil-core.def",'w') do |f|
 #       symfile.write(#{a[0]}=#{a[1]}\n")
