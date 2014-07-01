d9573c559b1c74b342691133989bfdec4a8a73df
Charlie Hitselberger <chitselb@gmail.com>
2014-06-30 22:39:27 -0400

    LOAD-BUFFERS works!

--- xap.rb
+++ xap.rb
@@ -286,7 +286,7 @@ _semi
     def write_core_defs(outputfile,labels)
         # these labels have an address that conflicts with a keyword,
         # e.g. addresses containing 'bc' or 'add' used by Sweet-16
-        bogus = " usersp0 userrp0 slashmod put umstar twodup type01 _emptybuffers _pdot _savebuffers _number "
+        bogus = " usersp0 userrp0 slashmod put umstar twodup type01 _emptybuffers _pdot _savebuffers _number _setnam "
         always_use_decimal = false
         symfile = File.open("./build/"+outputfile,'w') do |file|
             labels.each do |label, addr|
