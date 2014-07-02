136a9fc196d5b11b6df953fe0daa2732f0d6e5cf
Charlie Hitselberger <chitselb@gmail.com>
2014-07-02 01:45:26 -0400

    ...

--- xap.rb
+++ xap.rb
@@ -286,7 +286,7 @@ _semi
     def write_core_defs(outputfile,labels)
         # these labels have an address that conflicts with a keyword,
         # e.g. addresses containing 'bc' or 'add' used by Sweet-16
-        bogus = " usersp0 userrp0 slashmod put umstar twodup type01 _emptybuffers _pdot _savebuffers _number _setnam dnegate "
+        bogus = " usersp0 userrp0 slashmod put umstar twodup type01 _emptybuffers _pdot _savebuffers _number _setnam dnegate number07 _ddotr key01 key _question unpkt02 dots03 dots04 emitvector mult max01 rlencode loadbuffers02 "
         always_use_decimal = false
         symfile = File.open("./build/"+outputfile,'w') do |file|
             labels.each do |label, addr|
