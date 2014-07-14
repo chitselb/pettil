238df5fdd4f2def083fcd7e96ad0a9e3a540de4b
Charlie Hitselberger <chitselb@gmail.com>
2014-07-14 18:30:09 -0400

    squeezing tool

---
+++ suchen.rb
@@ -0,0 +1,21 @@
+#!/home/chitselb/bin/ruby
+# suchen.rb
+#
+# Analyzes a hexdump of the latest build, searching for duplicate strings
+# that might be factorable
+#
+minimum_size=16
+
+all = File.binread("build/pettil.obj")
+
+i = 0
+(all.size-minimum_size).times do
+    seek = all[i+=1, minimum_size]
+    j = all.index(seek,i+1)
+    if !j.nil?
+        seek.each_byte {|b| print b.to_s(16).rjust(2,'0')," "}
+        print   " ", (i+0x6bfc).to_s(16).rjust(4,'0'), \
+                " ", (j+0x6bfc).to_s(16).rjust(4,'0'), "\n"   
+    end
+end
+
