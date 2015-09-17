30d5045e92ea4816527a03d24d284dc1b7768336
Charlie Hitselberger <chitselb@gmail.com>
2015-09-17 16:36:53 -0400

    ''

--- xap.rb
+++ xap.rb
@@ -326,7 +326,7 @@ _semi
 
 
 
-        bogus = " brktoggle rlencode01 docconst01 dplus cr betweenq01 pleave rlencode04 "
+        bogus = " brktoggle rlencode01 docconst01 dplus cr betweenq01 pleave rlencode04 rlencode07 "
 
 
 
