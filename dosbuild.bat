5a0c3dd97e7998e7cd8af53a1a5e2f8837efab74
Charlie Hitselberger <chitselb@gmail.com>
2016-08-15 12:42:12 -0400

    dosbuild now makes docs\sizes.txt

--- dosbuild.bat
+++ dosbuild.bat
@@ -13,12 +13,22 @@ copy/y/b pettil-core.obj+pettil-tdict.obj+pettil.sym  pettil.obj
 type pettil.mon | sort >t.t
 copy/y t.t+..\pettil.dbg pettil.mon
 rem c1541 -format pettil,pt d64 pettil.d64 -attach pettil.d64 -write pettil.obj pettil
+set f=pettil-core.obj
+for /F "tokens=4* delims= " %%A IN ('DIR %f% /-C /N ^| FIND /I "%f%"') do set S=        %%A
+echo %S:~-8% %f%> ..\docs\sizes.txt
+set f=pettil.obj
+for /F "tokens=4* delims= " %%A IN ('DIR %f% /-C /N ^| FIND /I "%f%"') do set S=        %%A
+echo %S:~-8% %f%>> ..\docs\sizes.txt
+set f=pettil-tdict.obj
+for /F "tokens=4* delims= " %%A IN ('DIR %f% /-C /N ^| FIND /I "%f%"') do set S=        %%A
+echo %S:~-8% %f%>> ..\docs\sizes.txt
+set f=pettil.sym
+for /F "tokens=4* delims= " %%A IN ('DIR %f% /-C /N ^| FIND /I "%f%"') do set S=        %%A
+echo %S:~-8% %f%>> ..\docs\sizes.txt
 cd ..
 mkdir -p ./tmp/tiddlypettil/tiddlers
 copy .\docs\statictiddlers\tiddlywiki.info .\tmp\tiddlypettil\
 copy .\docs\statictiddlers\*.tid .\tmp\tiddlypettil\tiddlers\
 set mydate=doc generated %date:~10,4%-%date:~4,2%-%date:~7,2%
 sed "s/datetimestamp/%mydate%/" ./docs/statictiddlers/AboutPETTIL.tid >./tmp/tiddlypettil/tiddlers/AboutPETTIL.tid
-cd .\tmp\tiddlypettil
-tiddlywiki --load ../pettil.json --output ../../docs/ --rendertiddler $:/core/save/all tiddlypettil.html text/plain 
-cd ..\..
+cd .\tmp\tiddlypettil & tiddlywiki --load ../pettil.json --output ../../docs/ --rendertiddler $:/core/save/all tiddlypettil.html text/plain & cd ..\..
