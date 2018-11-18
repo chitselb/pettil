93ca5e66e80e90ee1d9f5264f54e213250edae53
Charlie Hitselberger <chitselb@gmail.com>
2018-11-18 10:58:40 -0500

    ...

---
+++ docs/card.mm
@@ -0,0 +1,99 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="card" FOLDED="false" ID="ID_1510128541" CREATED="1541733670688" MODIFIED="1541735986262" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="13" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="display card in hand" POSITION="right" ID="ID_391128499" CREATED="1541735938247" MODIFIED="1541736008277" HGAP_QUANTITY="61.24999859184031 pt" VSHIFT_QUANTITY="-21.74999935179951 pt">
+<edge COLOR="#7c007c"/>
+</node>
+<node TEXT="handle rules" POSITION="right" ID="ID_1119965471" CREATED="1541734951179" MODIFIED="1541736018178" HGAP_QUANTITY="62.749998547136826 pt" VSHIFT_QUANTITY="35.24999894946818 pt">
+<edge COLOR="#ff0000"/>
+<node TEXT="off topic" ID="ID_1512205846" CREATED="1541733691697" MODIFIED="1541735040398" HGAP_QUANTITY="42.49999915063384 pt" VSHIFT_QUANTITY="-7.499999776482589 pt">
+<node TEXT="&gt; value" ID="ID_180407000" CREATED="1541733702723" MODIFIED="1541735118168">
+<node TEXT="illegal" ID="ID_442450684" CREATED="1541733815830" MODIFIED="1541735118164" HGAP_QUANTITY="25.999999642372142 pt" VSHIFT_QUANTITY="8.249999754130847 pt"/>
+</node>
+<node TEXT="&lt;= value" ID="ID_524032944" CREATED="1541733830837" MODIFIED="1541735113895">
+<node TEXT="small talk" ID="ID_1463713421" CREATED="1541733837442" MODIFIED="1541735113895" HGAP_QUANTITY="16.999999910593036 pt" VSHIFT_QUANTITY="12.7499996200204 pt">
+<node TEXT="TV = 0" ID="ID_312113449" CREATED="1541735196895" MODIFIED="1541735200159"/>
+</node>
+</node>
+</node>
+<node TEXT="on topic" ID="ID_1929192916" CREATED="1541733678478" MODIFIED="1541735035479" HGAP_QUANTITY="38.74999926239255 pt" VSHIFT_QUANTITY="4.499999865889556 pt">
+<node TEXT="&gt;= value" ID="ID_1858179150" CREATED="1541733764756" MODIFIED="1541735124241">
+<node TEXT="lead conversation +1" ID="ID_452458458" CREATED="1541733785381" MODIFIED="1541735124238" HGAP_QUANTITY="16.999999910593036 pt" VSHIFT_QUANTITY="-11.999999642372142 pt"/>
+<node TEXT="T(VS) = card" ID="ID_734285318" CREATED="1541735207740" MODIFIED="1541735235679"/>
+</node>
+<node TEXT="&lt; value" ID="ID_1594103376" CREATED="1541733774283" MODIFIED="1541735127415">
+<node TEXT="follow conversation +0" ID="ID_360933648" CREATED="1541733796071" MODIFIED="1541735127413" HGAP_QUANTITY="27.499999597668662 pt" VSHIFT_QUANTITY="-14.249999575316918 pt"/>
+<node TEXT="T(VS) = card" ID="ID_1036443945" CREATED="1541735213233" MODIFIED="1541735252713"/>
+</node>
+</node>
+<node TEXT="change topic" ID="ID_438667004" CREATED="1541733846522" MODIFIED="1541735132727" HGAP_QUANTITY="57.499998703599026 pt" VSHIFT_QUANTITY="8.999999731779106 pt">
+<node TEXT="two cards" ID="ID_862450917" CREATED="1541733856964" MODIFIED="1541735132727" HGAP_QUANTITY="25.99999964237214 pt" VSHIFT_QUANTITY="-6.749999798834329 pt">
+<node TEXT="+1" ID="ID_220506562" CREATED="1541733866937" MODIFIED="1541733870289"/>
+</node>
+</node>
+</node>
+</node>
+</map>
