b2cca2f6a18c36460501bda9696b4c5fd59b38e2
Charlie Hitselberger <chitselb@gmail.com>
2019-05-22 04:08:03 -0400

    ...

--- tools/sql3.sh
+++ tools/sql3.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sqlite3 test.db <<EOF
 -- create table n (id INTEGER PRIMARY KEY,f TEXT,l TEXT);
-insert into n (f,l) values ('john','smith');
-select * from n;
+-- insert into n (f,l) values ('joe','potatonose');
+select json_object('id', id, 'f', f, 'l', l) from n where id=$1;
+-- select json_array('f', f, 'l', l) from n;
 EOF
